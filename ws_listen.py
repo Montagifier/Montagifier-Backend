59e6b5e766a03430ad19acdc15f90efca150bbab
Michael Anthony Knyszek <mknyszek@berkeley.edu>
2015-10-11 06:31:36 -0700

    switched to simpler library

--- ws_listen.py
+++ ws_listen.py
@@ -1,108 +1,67 @@
-import asyncio
-import websockets
-import time
-import threading
-from queue import Queue
+import time, queue, threading
+from websocket_server import WebsocketServer
+from socketserver import ThreadingMixIn, TCPServer
 from collections import deque, OrderedDict
 from util import Video, Sound, Skip, CheckIn, CheckOut
 
-CONN_MAX = 2048 
 QUEUE_MAX = 2048 
 
-class State:
-    courier = None
-    connections = OrderedDict()
+skip = 0
+v_time = 0
+mb_users = 0
+videos = deque()
+
+def next_video(server):
+    global v_time, skip, videos
+    if videos:
+        videos.popleft()
+        server.send_message_to_all(str(videos[0]))
+    v_time = 0
     skip = 0
-    time = 0
-    mb_users = 0
-    videos = deque()
 
-    @classmethod
-    def register(cls, websocket):
-        if len(cls.connections) > CONN_MAX:
-            websocket.close()
-            return False
-        cls.connections[websocket] = Queue()
-        return True
-
-    @classmethod
-    def unregister(cls, websocket):
-        del cls.connections[websocket]
-
-    @classmethod
-    def broadcast(cls, msg):
-        for conn, queue in cls.connections.items():
-            queue.put(msg)
-
-    @classmethod
-    def next_video(cls):
-        if cls.videos:
-            cls.videos.popleft()
-            cls.broadcast(cls.videos[0])
-        cls.time = 0
-        cls.skip = 0
-
-@asyncio.coroutine
-def handler(websocket, path):
-    if State.videos:
-        yield from websocket.send(str(videos[0]))
-    if State.register(websocket):
-        while True:
-            update = None
-            if not State.connections[websocket].empty():
-                update = State.connections[websocket].get(block=False)
-            else:
-                continue
-                
-            if type(update) in (Video, Sound):
-                yield from websocket.send(str(update))
-
-            if not websocket.open:
-                break
-        State.unregister(websocket)
-
-def updater():
+def updater(courier, server):
+    global v_time, skip, videos, mb_users
     while True:
-        if State.videos and time > State.videos[0].duration:
-            State.next_video()
+        if videos and v_time > videos[0].duration:
+            next_video(server)
 
-        req = None
-        if not State.courier.empty():
-            req = State.courier.get(block=False)
-        else:
-            continue
+        req = courier.get()
 
         if type(req) is Video and len(videos) < QUEUE_MAX:
-            State.videos.append(req)
+            videos.append(req)
         elif type(req) is Sound:
-            State.broadcast(req)
+            server.send_message_to_all(str(req))
         elif type(req) is Skip:
-            State.skip += 1
-            if State.skip > (len(State.connections) + State.mb_users) / 2:
-                State.next_video()
+            skip += 1
+            if skip > (len(server.clients) + mb_users) / 2:
+                next_video(server)
         elif type(req) is CheckIn:
-            State.mb_users += 1
+            mb_users += 1
         elif type(req) is CheckOut:
-            State.mb_users -= 1
+            if mb_users > 0:
+                mb_users -= 1
 
-        time.sleep(1)
-        State.time += 1
+
+def v_timer(server):
+    global v_time, videos
+    while True:
+        if videos:
+            time.sleep(1)
+            v_time += 1
+
 
 def listen(courier, host, port):
     import prctl, signal
     prctl.set_pdeathsig(signal.SIGKILL)
 
-    State.courier = courier
-    uthread = threading.Thread(target=updater, daemon=True)
+    server = WebsocketServer(port)
+
+    uthread = threading.Thread(target=updater, args=(courier, server), daemon=True)
+    tthread = threading.Thread(target=v_timer, args=(server,), daemon=True)
+
     uthread.start()
+    tthread.start()
 
     print("Starting WebSocket Server...")
-    start_server = websockets.serve(handler, host, port)
-    try:
-        asyncio.get_event_loop().run_until_complete(start_server)
-        asyncio.get_event_loop().run_forever()
-    except KeyboardInterrupt:
-        return
-
-    uthread.join()
-
+    server.run_forever()
+    
