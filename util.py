458b84468c41c843a892e442f1212e68929153e4
Michael Anthony Knyszek <mknyszek@berkeley.edu>
2015-10-11 03:31:15 -0700

    updated youtube stuff

--- util.py
+++ util.py
@@ -4,23 +4,31 @@ from datetime import timedelta
 from urllib.request import urlopen
 from xml.dom.minidom import parseString
 
-YT_URL = 'https://gdata.youtube.com/feeds/api/videos/{0}?v=2i&alt=jsonc'
+YT_URL = "https://www.googleapis.com/youtube/v3/videos?id={0}&key={1}&part=contentDetails" 
+
+with open('yt_key', 'r') as f:
+    YT_KEY = f.read()
 
 class Video:
     def __init__(self, link):
+        self.kind = 'video'
         self.link = link
         try:
-            response = urlopen(YT_URL.format(link)).read()
+            print(YT_URL.format(link, YT_KEY))
+            response = urlopen(YT_URL.format(link, YT_KEY)).read()
+            print(response)
             obj = json.loads(response)
-            self.duration = int(obj['duration'])
-        except:
+            self.duration = int(obj['videos'][1]['contentDetails'])
+        except Exception as e:
             self.duration = 0
+            print("Youtube Error: " + str(e))
 
     def __str__(self):
         return json.dumps(self.__dict__)
 
 class Sound:
     def __init__(self, category, name):
+        self.kind = 'sound'
         self.category = category
         self.name = name
 
